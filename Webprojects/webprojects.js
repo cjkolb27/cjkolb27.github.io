53d6d175fbfe4eceb23f2467f465494d97aa521e
cjkolb <cjkolb27@gmail.com>
2026-03-20 22:46:35 -0400

    Updated web projects tab and created chess template

---
+++ Webprojects/webprojects.js
@@ -0,0 +1,41 @@
+const projects_div = document.querySelector(".all-projects");
+
+async function getData(link) {
+    const res = await fetch(link);
+    return res.json();
+}
+
+const jsonData = await getData("webprojects.json");
+console.log(jsonData);
+jsonData.forEach(data => {
+    const d1 = document.createElement("div");
+    d1.classList.add("proj");
+    d1.addEventListener("click", () => {
+        window.location.href = `${data.link}`;
+    });
+
+    const d2 = document.createElement("div");
+    d2.classList.add("top-div");
+
+    const p1 = document.createElement("p");
+    p1.textContent = data.name;
+    p1.classList.add("p-title");
+    d2.appendChild(p1);
+
+    const img = document.createElement("img");
+    img.src = data.file;
+    img.classList.add("preview-img");
+    d2.appendChild(img);
+
+    const d3 = document.createElement("div");
+    d3.classList.add("bottom-div");
+
+    const p2 = document.createElement("p");
+    p2.textContent = data.description;
+    p2.classList.add("p-desc");
+    d3.appendChild(p2);
+
+    d1.appendChild(d2);
+    d1.appendChild(d3);
+    projects_div.appendChild(d1);
+});
