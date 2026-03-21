27569490c27676a10053307c8dfbac080c15cdcb
cjkolb <cjkolb27@gmail.com>
2026-03-21 00:43:23 -0400

    Chess pieces populate the board

--- Webprojects/Chess/chess.js
+++ Webprojects/Chess/chess.js
@@ -18,11 +18,48 @@ window.addEventListener("beforeunload", (event) => {
 
 const white = [];
 const black = [];
+const startRow = [5, 4, 3, 2, 1, 3, 4, 5];
+const rowChars = ["A", "B", "C", "D", "E", "F", "G", "H"];
+let temp = 0;
 
 if (Math.random() < .5) {
     console.log("White");
-    const you = 0;
+    temp = 0;
 } else {
     console.log("Black");
-    const you = 1;
+    temp = 1;
+}
+const you = temp;
+loadPieces(you);
+
+function loadPieces(start) {
+    let b = start == 0 ? true : false;
+    for (let i = 0; i < 16; i++) {
+        if (i == 8) {
+            b = !b;
+        }
+        const square1 = document.querySelector(`#${rowChars[i < 8 ? i : i - 8]}${i < 8 ? "1" : "8"}`);
+        const canvas1 = document.createElement("canvas");
+        canvas1.classList.add("piece");
+        const w = 213;
+        const h = 213;
+        canvas1.width = w;
+        canvas1.height = h;
+        const ctx1 = canvas1.getContext("2d");
+        ctx1.drawImage(spriteSheet, w * (startRow[i < 8 ? i : i - 8] - 1), b ? 0 : w + 1, w, h, 0, 0, w, h);
+        square1.appendChild(canvas1);
+        b ? white.push({ "id": i < 8 ? i : i - 8, "c": canvas1, "t": startRow[i < 8 ? i : i - 8], "l": `${rowChars[i < 8 ? i : i - 8]}${i < 8 ? "1" : "8"}`}) : black.push({ "id": i < 8 ? i : i - 8, "c": canvas1, "t": startRow[i < 8 ? i : i - 8], "l": `${rowChars[i < 8 ? i : i - 8]}${i < 8 ? "1" : "8"}`});
+
+        const square2 = document.querySelector(`#${rowChars[i < 8 ? i : i - 8]}${i < 8 ? "2" : "7"}`);
+        const canvas2 = document.createElement("canvas");
+        canvas2.classList.add("piece");
+        canvas2.width = w;
+        canvas2.height = h;
+        const ctx2 = canvas2.getContext("2d");
+        ctx2.drawImage(spriteSheet, w * 5, b ? 0 : w + 1, w, h, 0, 0, w, h);
+        square2.appendChild(canvas2);
+        b ? white.push({ "id": i < 8 ? i + 8 : i, "c": canvas2, "t": 6, "l": `${rowChars[i < 8 ? i : i - 8]}${i < 8 ? "2" : "7"}`}) : black.push({ "id": i < 8 ? i + 8 : i, "c": canvas1, "t": 6, "l": `${rowChars[i < 8 ? i : i - 8]}${i < 8 ? "2" : "7"}`});
+    }
+    console.log(white);
+    console.log(black);
 }
